c06df7ea7bea3ca5c9decd0faf2c15ca3ac090b6
Ukidabek <ukidabek@gmail.com>
2018-07-07 23:25:32 +0200

    - Show hide for ID added.

---
+++ Assets/Content/Code/NPC/Editor/NPCIdGUIEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace NPCs
+{
+
+    [CustomEditor(typeof(NPCIdGUI))]
+    public class NPCIdGUIEditor : Editor
+    {
+        [SerializeField] private NPCIdGUI gui = null;
+
+        private void OnEnable()
+        {
+            gui = target as NPCIdGUI;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if(GUILayout.Button("Show"))
+                gui.Show();
+
+            if(GUILayout.Button("Hide"))
+                gui.Hide();
+        }
+    }
+}
