cb3a9ea40646c01eb00e2be949d4fcfb7f13c83f
Ukidabek <ukidabek@gmail.com>
2018-07-07 22:24:18 +0200

    - PauseManager added.

---
+++ Assets/Content/Code/Game/Editor/PauseManagerEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PauseManager))]
+public class PauseManagerEditor : Editor
+{
+    private PauseManager manager = null;
+
+    private void OnEnable()
+    {
+        manager = target as PauseManager;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (Time.timeScale == 0f)
+        {
+            if (GUILayout.Button("Play"))
+                manager.Play();
+        }
+        else
+        {
+            if (GUILayout.Button("Pause"))
+                manager.Pouse();
+        }
+    }
+}
---
+++ Assets/Content/Code/Game/PauseManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public static PauseManager Instance { get; private set; }
+
+    private float _timeScale = 0;
+    private float _timeStemp = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this.gameObject);
+
+        _timeScale = Time.timeScale;
+        _timeStemp = Time.maximumDeltaTime;
+    }
+
+    public void Pouse()
+    {
+        Time.timeScale= 0;
+        Time.maximumDeltaTime = 0;
+        GameplayEvents.NotifyOnLockPlayerInput();
+    }
+
+    public void Play()
+    {
+        Time.timeScale = _timeScale;
+        Time.maximumDeltaTime = _timeStemp;
+        GameplayEvents.NotifyOnUnlockPlayerInput();
+    }
+}
