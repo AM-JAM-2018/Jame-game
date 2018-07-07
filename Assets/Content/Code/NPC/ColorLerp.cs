a1537019b796aa074ece0c10de290d3968a0aa16
Ukidabek <ukidabek@gmail.com>
2018-07-07 22:44:45 +0200

    - Npc update.

---
+++ Assets/Content/Code/NPC/ColorLerp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorLerp : MonoBehaviour
+{
+    [SerializeField] private Image _image = null;
+    [SerializeField] private Color _a = new Color();
+    [SerializeField] private Color _b = new Color();
+
+    private void Awake()
+    {
+        _image.color = _a;
+    }
+
+    public void LerpColor(float fill)
+    {
+        _image.color = Color.Lerp(_a, _b, 1 - fill);
+    }
+}
