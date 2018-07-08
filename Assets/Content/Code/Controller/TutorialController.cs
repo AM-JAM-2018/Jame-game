fa314faf0cff9081dd9fe5b775d29ef01f681baa
Xornogard <Miller.Daniel91@yahoo.co.uk>
2018-07-08 11:02:51 +0200

    Tutorial w menu glownym.

---
+++ Assets/Content/Code/Controller/TutorialController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialController : MonoBehaviour {
+
+	public Image tutorialImage;
+	public GameObject nextImageButton;
+	public GameObject previousImageButton;
+
+
+	public List<Sprite> tutorialSprites;
+
+	private int currentTutorialSpritesIndex;
+
+	public void OnBeforeOpen()
+	{
+		currentTutorialSpritesIndex = 0;
+		previousImageButton.SetActive(false);
+		SetImage();
+	}
+
+	public void NextImage()
+	{
+		currentTutorialSpritesIndex++;
+		nextImageButton.SetActive(currentTutorialSpritesIndex + 1 < tutorialSprites.Count);
+		previousImageButton.SetActive(true);
+
+		SetImage();
+
+	}
+
+	public void PreviousImage()
+	{
+		currentTutorialSpritesIndex--;
+		previousImageButton.SetActive(currentTutorialSpritesIndex > 0);
+		nextImageButton.SetActive(true);
+
+		SetImage();
+	}
+
+	private void SetImage()
+	{
+		tutorialImage.sprite = tutorialSprites[currentTutorialSpritesIndex];
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
